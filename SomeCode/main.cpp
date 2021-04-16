113de57432cace6b607f33054f759a02c28b2b19
hantarr01 <thunderbolt655@mail.ru>
2021-04-16 16:29:59 +0300

    16.04.2021

--- SomeCode/main.cpp
+++ SomeCode/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <windows.h>
 
 int recursion1(int x,int y){
     if(x >= y){
@@ -30,6 +31,7 @@ int counter(){
         }
         std::cout << "|" << std::endl;
         n = line.length();
+        std::cout<<n<<"<-----"<<std::endl;
     }
 
     char s = line[n];
@@ -52,11 +54,66 @@ int counter(){
 return 0;
 }
 
+int brackets(){
+    std::string line;
+    int n = 0, l = 0, r = 0;
+
+    std::ifstream in("someScript.txt");
+    if (in.is_open()){
+        while (getline(in, line)){
+            std::cout << line <<std::endl;
+        }
+    }
+
+    in.close();
+
+    std::ifstream in_("someScript.txt");
+
+    while (!in_.eof()){
+        char *buffer = (char *)calloc(1, sizeof(char));
+        in_.read(buffer, 1);
+
+        if (*buffer == '{'){
+             l++;
+        }else if(*buffer == '}'){
+             r++;
+        }
+        delete buffer;
+    }
+
+     std::cout<<l<<" "<<r<<std::endl;
+
+    if(l == r){
+        std::cout<<std::endl;
+        std::cout<< "The balance of brackets is fine" <<std::endl;
+    }else{
+        std::cout<<std::endl;
+        std::cout<< "The balance of brackets is not fine" <<std::endl;
+    }
+
+    in_.close();
+
+    std::ofstream out;
+    out.open("result.txt");
+    if (out.is_open()){
+        if(l == r){
+            out<< "The balance of brackets is fine" <<std::endl;
+        }else{
+            out<< "The balance of brackets is not fine" <<std::endl;
+        }
+    }
+
+return 0;
+}
+
+
 int main(){
     recursion1(5,1);
     std::cout<<std::endl;
     recursion2(1,5);
     std::cout<<std::endl;
     counter();
+    std::cout<<std::endl;
+    brackets();
     return 0;
 }
